4e8c76196b26a0686ce555dd931a1c857d23e3f5
Ryan <citizen.townshend@gmail.com>
2026-07-14 15:56:37 -0300

    initial import for box cutter

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+Commandline image cropping tool.
---
+++ box_cutter/box_cutter.py
@@ -0,0 +1,51 @@
+import sys
+from PIL import Image
+from pathlib import Path
+
+import click
+
+
+def process_folder(folder: Path, output_folder: Path, box: tuple[int]) -> None:
+    print(f"Processing folder {folder}")
+    for item in folder.iterdir():
+        print(item)
+        process_file(item, output_folder, box)
+
+
+def process_file(filename: Path, output_folder: Path, box: tuple[int]) -> None:
+    print(f"Processing file {filename}")
+    with Image.open(filename) as img:
+        product = output_folder.joinpath(filename.name)
+        cropped = img.crop(box)
+        cropped.save(product)
+
+
+@click.command()
+@click.option(
+    "--input",
+    "input_path",
+    type=click.Path(path_type=Path, dir_okay=False, file_okay=True),
+)
+@click.option(
+    "--output", "output_folder", type=click.Path(path_type=Path, dir_okay=True)
+)
+@click.option("--box", type=int, nargs=4, help="left upper right lower")
+def main(input_path: Path, output_folder: Path, box: tuple[int]) -> None:
+    input_path = input_path.expanduser()
+    output_folder = output_folder.expanduser()
+
+    if not input_path.exists():
+        print("Input path does not exist")
+        sys.exit(5)
+
+    if input_path.is_dir():
+        folder = input_path.expanduser()
+        process_folder(folder, output_folder, box)
+    else:
+
+        filename = input_path.expanduser()
+        process_file(filename, output_folder, box)
+
+
+if __name__ == "__main__":
+    main()
