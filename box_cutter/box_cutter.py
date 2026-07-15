bca4f85d5c453a653c9ace63e3a3e4990a57932f
Ryan <citizen.townshend@gmail.com>
2026-07-15 14:17:21 -0300

    added loguru

--- box_cutter/box_cutter.py
+++ box_cutter/box_cutter.py
@@ -1,19 +1,19 @@
 import sys
 from PIL import Image
 from pathlib import Path
+from loguru import logger
 
 import click
 
 
 def process_folder(folder: Path, output_folder: Path, box: tuple[int]) -> None:
-    print(f"Processing folder {folder}")
+    logger.info(f"Processing folder {folder}")
     for item in folder.iterdir():
-        print(item)
         process_file(item, output_folder, box)
 
 
 def process_file(filename: Path, output_folder: Path, box: tuple[int]) -> None:
-    print(f"Processing file {filename}")
+    logger.info(f"Processing file {filename}")
     with Image.open(filename) as img:
         product = output_folder.joinpath(filename.name)
         cropped = img.crop(box)
@@ -33,11 +33,16 @@ def process_file(filename: Path, output_folder: Path, box: tuple[int]) -> None:
 )
 @click.option("--box", type=int, nargs=4, help="left upper right lower")
 def main(input_path: Path, output_folder: Path, box: tuple[int]) -> None:
-    input_path = input_path.expanduser()
-    output_folder = output_folder.expanduser()
+
+    try:
+        input_path = input_path.expanduser()
+        output_folder = output_folder.expanduser()
+    except Exception as error:
+        logger.error(error)
+        sys.exit(4)
 
     if not input_path.exists():
-        print("Input path does not exist")
+        logger.error("Input path does not exist")
         sys.exit(5)
 
     if input_path.is_dir():
